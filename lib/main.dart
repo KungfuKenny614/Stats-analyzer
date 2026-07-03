f229ee1e66b0bb7f1fc76edcd20efa325576593e
KungfuKenny614 <KungfuKenny614@users.noreply.github.com>
2026-07-03 01:24:25 -0400

    Fix: Update package name to valid Dart identifier

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Stat Analyzer',
+      title: 'Stats Analyzer',
       theme: ThemeData.dark(),
       home: const Scaffold(
         body: Center(
